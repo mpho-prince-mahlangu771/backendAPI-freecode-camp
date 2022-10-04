9939c348026f83e1543f2fe6f9864130f890fc54
mpho prince mahlangu <mphomahlangu771@gmail.com>
2022-10-04 08:15:12 +0200

    added a get route handler to myApp.js

--- basic-node-and-express/myApp.js
+++ basic-node-and-express/myApp.js
@@ -1,7 +1,13 @@
 let express = require('express');
 let app = express();
 
+//hit http://localhost:3000/ on a browser
+app.get('/', (req, res) =>{
+    res.send("Hello Express");
+});
+
 console.log("Hello World");
+
 
 
 

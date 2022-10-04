d84c34a3c81f32f24b0eb0e76328bd06fae5a2b7
mpho prince mahlangu <mphomahlangu771@gmail.com>
2022-10-04 09:02:23 +0200

    mounted a middleware to http use() route handler

--- basic-node-and-express/myApp.js
+++ basic-node-and-express/myApp.js
@@ -1,24 +1,24 @@
 let express = require('express');
 let app = express();
 
-/*
-    order matters - if 1+ routes have same http method and route,
-    then the first one is choosen always
-*/
+//order matters - if 1+ routes have same http method and route,
+//then the first one is choosen always
+
 
 //serving a html file 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html');
 });
-
 
 //serving a plain string
 app.get('/', (req, res) =>{
     res.send("Hello Express");
 });
 
+//serving static assets
+app.use('/public', express.static(__dirname + '/public'));
 
-console.log("Hello World");
+console.log(__dirname + '/public');
 
 
 

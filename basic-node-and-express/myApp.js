3b735825d3c9bad92bb97d75cc56bc9cd7f9a035
mpho prince mahlangu <mphomahlangu771@gmail.com>
2022-10-04 08:32:28 +0200

    sent a file as a response to get

--- basic-node-and-express/myApp.js
+++ basic-node-and-express/myApp.js
@@ -1,10 +1,22 @@
 let express = require('express');
 let app = express();
 
-//hit http://localhost:3000/ on a browser
+/*
+    order matters - if 1+ routes have same http method and route,
+    then the first one is choosen always
+*/
+
+//serving a html file 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+});
+
+
+//serving a plain string
 app.get('/', (req, res) =>{
     res.send("Hello Express");
 });
+
 
 console.log("Hello World");
 

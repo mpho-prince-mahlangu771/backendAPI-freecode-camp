807c0edad5dcfe20c789bd04d3c8f02ae058f75c
mpho prince mahlangu <mphomahlangu771@gmail.com>
2022-10-05 22:20:59 +0200

    added a middleware to myApp.js

--- basic-node-and-express/myApp.js
+++ basic-node-and-express/myApp.js
@@ -1,9 +1,19 @@
+//loading the variable in the .env file
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
 //order matters - if 1+ routes have same http method and route,
 //then the first one is choosen always
 
+/* 
+For every request,the middleware logs to the console a string taking the following format: method path - ip
+an example would look like this: GET /json - ::ffff:127.0.0.1
+*/
+app.use((req,res,next) => {
+    console.log(req.method + " " +  req.originalUrl + " - " + req.ip );
+    next();
+});
 
 //serving a html file 
 app.get('/', (req, res) => {
@@ -18,10 +28,15 @@ app.get('/', (req, res) =>{
 //serving static assets
 app.use('/public', express.static(__dirname + '/public'));
 
-//json response
+
+
+
+/*
+//serving a json response
 app.get('/json', (req, res) => {
     res.json({"message": "Hello json" })
 });
+*/
 
 console.log(__dirname + '/public');
 

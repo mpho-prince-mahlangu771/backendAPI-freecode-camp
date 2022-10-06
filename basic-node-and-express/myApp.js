987246f761cfc8444d92c506fb9e8057c7e27e94
mpho prince mahlangu <mphomahlangu771@gmail.com>
2022-10-06 15:36:36 +0200

    added a chain-middleware to myApp.js

--- basic-node-and-express/myApp.js
+++ basic-node-and-express/myApp.js
@@ -6,6 +6,8 @@ let app = express();
 //order matters - if 1+ routes have same http method and route,
 //then the first one is choosen always
 
+
+
 /* 
 For every request,the middleware logs to the console a string taking the following format: method path - ip
 an example would look like this: GET /json - ::ffff:127.0.0.1
@@ -23,6 +25,15 @@ app.get('/', (req, res) => {
 //serving a plain string
 app.get('/', (req, res) =>{
     res.send("Hello Express");
+});
+
+//Chaining Middleware to Create a Time Server
+//to pass testcases enter: http://localhost:3000/ without the /now path
+app.get('/now', (req,res,next) => {
+    req.time = new Date().toString();
+    next();
+}, (req,res) => {
+    res.send({time: req.time});
 });
 
 //serving static assets
